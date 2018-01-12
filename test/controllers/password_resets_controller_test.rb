60896b05ad9eaf97963dcab515d16f9d6fb85c66
kazuya nakajima <uniss010@gmail.com>
2018-01-12 16:23:23 +0900

    pass password_resets_controller_test

--- test/controllers/password_resets_controller_test.rb
+++ test/controllers/password_resets_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
   def setup 
+    ActionMailer::Base.deliveries.clear
     @user = users(:michael)
   end
   test "should get new" do
@@ -10,7 +11,15 @@ class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
-    get password_resets_edit_url
+    get new_password_reset_path
+    assert_template "password_resets/new"
+    post password_resets_path, 
+      params: { password_reset: { email: @user.email } }
+    assert_not_equal @user.reset_digest, @user.reload.reset_digest
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    assert_redirected_to root_url
+    user = assigns(:user)
+    get edit_password_reset_path(user.reset_token, email: user.email)
     assert_response :success
   end
 end
