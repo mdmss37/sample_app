c1baa43883ffa0fa0b6a9661e0ec7a82351df2b5
kazuya nakajima <uniss010@gmail.com>
2018-01-15 18:24:30 +0900

    finished 13.3.2

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -4,4 +4,19 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @micropost = microposts(:orange)
+  end
+  test "shold redirect create when not logged in" do
+    assert_no_difference "Micropost.count" do
+      post microposts_path, params: { micropost: { content: "Lorem ipsum"}}
+    end
+    assert_redirected_to login_url
+  end
+  test "shold redirect destroy when not logged in" do
+    assert_no_difference "Micropost.count" do
+      delete micropost_path(@micropost)
+    end
+    assert_redirected_to login_url
+  end
 end
